2f726cb4ae8c6649c02b7a6d3e2a7f09dcd8805e
JunsuPark <junsu1222@naver.com>
2024-08-13 17:19:11 +0900

    feat : 도메인 설계 (#2)

---
+++ src/main/java/flow/practice/blockfileextentions/domain/entity/BlockedFileExtension.java
@@ -0,0 +1,20 @@
+package flow.practice.blockfileextentions.domain.entity;
+
+import flow.practice.blockfileextentions.global.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "blocked_file_extension")
+public class BlockedFileExtension extends BaseEntity {
+
+    @Column(name = "name", length = 20, nullable = false)
+    private String name;
+
+}
---
+++ src/main/java/flow/practice/blockfileextentions/global/entity/BaseEntity.java
@@ -0,0 +1,35 @@
+package flow.practice.blockfileextentions.global.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import java.time.ZonedDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@EntityListeners(value = {AuditingEntityListener.class})
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private ZonedDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private ZonedDateTime updatedAt;
+
+    @Column(name = "deleted_at", nullable = false)
+    private ZonedDateTime deletedAt;
+}

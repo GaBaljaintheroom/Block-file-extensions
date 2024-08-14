1c1c5c8286a99c9ff31ef9c1ab7a95762b2c2835
JunsuPark <junsu1222@naver.com>
2024-08-14 18:01:37 +0900

    feat: 홈 뷰 구현 (#8)

---
+++ src/main/java/flow/practice/blockfileextentions/domain/controller/FileController.java
@@ -0,0 +1,16 @@
+package flow.practice.blockfileextentions.domain.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/file")
+public class FileController {
+
+    @GetMapping("/extensions")
+    public String home() {
+        return "home";
+    }
+
+}

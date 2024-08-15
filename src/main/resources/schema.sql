ef9dd75d1b2893b8d9edc23dcd845e6f340050d0
JunsuPark <junsu1222@naver.com>
2024-08-15 23:16:50 +0900

    feat: 고정 확장자 데이터 삽입 (#10)

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS blocked_file_extension CASCADE;
+
+CREATE TABLE blocked_file_extension
+(
+    is_fixed   BIT         NOT NULL,
+    created_at DATETIME(6) NOT NULL,
+    deleted_at DATETIME(6),
+    id         BIGINT      NOT NULL AUTO_INCREMENT,
+    updated_at DATETIME(6) NOT NULL,
+    name       VARCHAR(20) NOT NULL,
+    PRIMARY KEY (id)
+);
